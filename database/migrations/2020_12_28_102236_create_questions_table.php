4eede95866df7be518c16bc4cc5fcaf40b84a697
wngur6076 <wngur6076@naver.com>
2020-12-28 19:47:50 +0900

    Generate Question Model via artisan model generator

---
+++ database/migrations/2020_12_28_102236_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('answers')->default(0);
+            $table->integer('votes')->default(0);
+            $table->unsignedBigInteger('best_answer_id')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
