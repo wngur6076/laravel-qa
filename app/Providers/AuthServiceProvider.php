2d50791650f2662dca0a325156c4f874812a4209
wngur6076 <wngur6076@naver.com>
2020-12-29 20:37:44 +0900

    policies를 이용한 인증구현

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,6 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
+        'App\Models\Question' => 'App\Policies\QuestionPolicy',
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
