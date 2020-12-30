3a0feb7e444e8c8db5c805701fdd3ca279e4d27a
wngur6076 <wngur6076@naver.com>
2020-12-30 11:32:29 +0900

    Generate fake answers

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -25,4 +25,14 @@ class Answer extends Model
 
         return $markdown->convertToHtml($this->body);
     }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function($answer) {
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
+    }
 }
--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\Question;
+use App\Models\Answer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class QuestionFactory extends Factory
+class AnswerFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Question::class;
+    protected $model = Answer::class;
 
     /**
      * Define the model's default state.
@@ -22,11 +22,9 @@ class QuestionFactory extends Factory
     public function definition()
     {
         return [
-            'title' => rtrim($this->faker->sentence(rand(5, 10)), "."),
             'body' => $this->faker->paragraphs(rand(3, 7), true),
-            'views' => rand(0, 10),
-            // 'answers_count' => rand(0, 10),
-            'votes' => rand(-3, 10),
+            'user_id' => \App\Models\User::pluck('id')->random(),
+            'votes_count' => rand(0, 5),
         ];
     }
 }
