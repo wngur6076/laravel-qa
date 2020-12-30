ac616728d3deba7af32597bed8be798e6881a00c
wngur6076 <wngur6076@naver.com>
2020-12-30 15:34:17 +0900

    answer update구현

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Models\Question' => 'App\Policies\QuestionPolicy',
+        'App\Models\Answer' => 'App\Policies\AnswerPolicy',
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
