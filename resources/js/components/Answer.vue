43090cbd7c8bc52147dc9fb3b4fb5109f66bed2c
wngur6076 <wngur6076@naver.com>
2021-01-02 16:28:24 +0900

    Create vue answer component

---
+++ resources/js/components/Answer.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    props: ['answer'],
+
+    data() {
+        return {
+            editing: false,
+            body: this.answer.body,
+            bodyHtml: this.answer.body_html,
+            id: this.answer.id,
+            questionId: this.answer.question_id,
+            beforeEditCache: null
+        }
+    },
+
+    computed: {
+        isInvalid () {
+            return this.body.length < 10;
+        }
+    },
+
+    methods: {
+        edit () {
+            this.beforeEditCache = this.body;
+            this.editing = true;
+        },
+
+        cancel () {
+            this.body = this.beforeEditCache;
+            this.editing = false;
+        },
+
+        update () {
+            axios.patch(`/questions/${this.questionId}/answers/${this.id}`, {
+                body: this.body
+            })
+            .then(res => {
+                this.editing = false;
+                this.bodyhtml = res.data.body_html;
+                console.log(res.data.body_html);
+                alert(res.data.message);
+            })
+            .catch(err => {
+                alert(err.response.data.message);
+            })
+        }
+    },
+}
+</script>
--- resources/views/answers/_answer.blade.php
+++ resources/views/answers/_answer.blade.php
@@ -1,29 +1,42 @@
-<div class="media post">
-    @include('shared._vote', [
-        'model' => $answer
-    ])
-    <div class="media-body">
-        {!! $answer->body_html !!}
-        <div class="row">
-            <div class="col-4">
-                <div class="ml-auto">
-                    @can('update', $answer)
-                        <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                    @endcan
-                    @can('delete', $answer)
-                        <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="POST">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
-                    @endcan
+<answer :answer="{{ $answer }}" inline-template>
+    <div class="media post">
+        @include('shared._vote', [
+            'model' => $answer
+        ])
+        <div class="media-body">
+            <form v-if="editing" @submit.prevent="update">
+                <div class="form-group">
+                    <textarea v-model="body" class="form-control" rows="10" required></textarea>
                 </div>
-            </div>
-            <div class="col-4">
-            </div>
-            <div class="col-4">
-                <user-info :model="{{ $answer }}" label="Answered"></user-info>
+                {{-- <input type="submit" @click.prevent="update" value="Update"> --}}
+                <button class="btn btn-primary" :disabled="isInvalid">Update</button>
+                <button class="btn btn-outline-secondary" @click="cancel" type="button">Cancel</button>
+            </form>
+            <div v-else>
+                <div v-html="bodyHtml"></div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="ml-auto">
+                            @can('update', $answer)
+                                <a @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
+                            @endcan
+                            @can('delete', $answer)
+                                <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="POST">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                </form>
+                            @endcan
+                        </div>
+                    </div>
+                    <div class="col-4">
+                    </div>
+                    <div class="col-4">
+                        <user-info :model="{{ $answer }}" label="Answered"></user-info>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+</answer>
