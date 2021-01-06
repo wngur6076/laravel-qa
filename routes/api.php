c6d6f9e7f8492f1253715b5f2518e7b548ae5329
wngur6076 <wngur6076@naver.com>
2021-01-07 03:23:28 +0900

    Create Api endpoints for Authentication

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\VoteAnswerController;
 use App\Http\Controllers\Api\AnswersController;
+use App\Http\Controllers\Api\MyPostsController;
 use App\Http\Controllers\Api\FavoritesController;
 use App\Http\Controllers\Api\QuestionsController;
+use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\AcceptAnswerController;
-use App\Http\Controllers\Api\MyPostsController;
 use App\Http\Controllers\Api\VoteQuestionController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\QuestionDetailsController;
 
 /*
@@ -23,7 +24,9 @@ use App\Http\Controllers\Api\QuestionDetailsController;
 |
 */
 
-Route::post('/token', [LoginController::class, 'getToken']);
+Route::post('/login', [LoginController::class, 'store']);
+Route::delete('/logout', [LoginController::class, 'destroy'])->middleware('auth:api');
+Route::post('/register', RegisterController::class);
 
 Route::get('/questions', [QuestionsController::class, 'index']);
 Route::get('/questions/{question}/answers', [AnswersController::class, 'index']);
