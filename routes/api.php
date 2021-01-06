af23b30aed682a7036ede975c6d49ddbed8c2a04
wngur6076 <wngur6076@naver.com>
2021-01-06 18:14:56 +0900

    Create api endpoint for displaying all questions

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
@@ -16,6 +17,7 @@ use App\Http\Controllers\Auth\LoginController;
 */
 
 Route::post('/token', [LoginController::class, 'getToken']);
+Route::get('/questions', [QuestionController::class, 'index']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
